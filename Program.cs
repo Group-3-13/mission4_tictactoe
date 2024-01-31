6eae40b1a0fa601f69da94f8f4a51c0ddc69773f
gogden245 <go245@byu.edu>
2024-01-30 23:30:57 -0700

    layout

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Authors: Jakob Blosil, Ethan Hoopes, Oluchi Ochulo, Grace Ogden
+// Description: tic tac toe game
+
+
+// Welcome the user to the game
+Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+// Create a game board array to store the players’ choices
+
+// Ask each player in turn for their choice and update the game board array
+
+// Print the board by calling the method in the supporting class
+
+// Check for a winner by calling the method in the supporting class,
+// and notify the players when a win has occurred and which player won the game
+ 
---
+++ game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace mission4_tictactoe
+{
+    internal class game
+    {
+        // Receive the “board” array from the driver class
+
+        // Contain a method that prints the board based on the information passed to it
+
+        // Contain a method that receives the game board array as input
+        // and returns if there is a winner and who it was
+    }
+}
